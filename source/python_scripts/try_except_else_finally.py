46d5756d6aef5da4a634effc54bc86494f46fedf
Zhaohui Mei <mzh.whut@gmail.com>
2019-03-23 22:24:26 +0800

    add some exception example

---
+++ source/python_scripts/try_except_else_finally.py
@@ -0,0 +1,21 @@
+# Filename: try_except_else_finally.py
+# Author: meizhaohui
+def expt2(a, b):
+    try:
+        c = a/b
+        print('the value is:{}'.format(c))
+    except ZeroDivisionError:
+        print('程序出现异常，异常信息：被除数为0')
+    except TypeError:
+        print('程序出现异常，异常信息：参数a或b的类型不支持，仅支持float或int类型')
+expt2(4, 0)
+
+
+# def expt1(a, b):
+#     try:
+#         c = a/b
+#         print('the value is:{}'.format(c))
+#     except ZeroDivisionError:
+#         print('程序出现异常，异常信息：被除数为0')
+# 
+# expt1(4,'')
